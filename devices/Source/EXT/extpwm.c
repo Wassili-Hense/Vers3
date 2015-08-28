98ca058b8b8a15eb2510647966226a6f1747e4a3
comparator <comparator@gmx.de>
2015-08-28 16:34:35 +0200

    extpwm new version

--- devices/Source/EXT/extpwm.c
+++ devices/Source/EXT/extpwm.c
@@ -33,89 +33,47 @@ bool pwmCheckSubidx(subidx_t * pSubidx)
     return true;
 }
 
-/*
-
-// Local Variables
-static const uint8_t pwm_port2cfg[EXTPWM_MAXPORT_NR] = EXTPWM_PORT2CFG;
-static const uint8_t pwm_port2dio[EXTPWM_MAXPORT_NR] = EXTPWM_PORT2DIO;
-
-// dio subroutines
-uint8_t dioCheckBase(uint16_t base);
-void dioConfigure(uint16_t base, eDIOmode_t Mode);
-
-// hal
-void hal_pwm_write(uint8_t Config, uint16_t value);
-
-// Local subroutines
-
-static uint8_t pwmCheckBase(uint16_t base)
-{
-    if((base >= EXTPWM_MAXPORT_NR) ||
-       (pwm_port2cfg[base] == 0xFF))        // Port not exist
-        return 2;
-        
-    return dioCheckBase(pwm_port2dio[base]);
-}
-
-// Check index
-uint8_t pwmCheckIdx(subidx_t * pSubidx)
-{
-    if((pSubidx->Type == objPinNPN) || (pSubidx->Type == objPinPNP))
-        return pwmCheckBase(pSubidx->Base);
-
-    return 2;
-}
-
 static e_MQTTSN_RETURNS_t pwmWriteOD(subidx_t * pSubidx, uint8_t Len, uint8_t *pBuf)
 {
+    // Prevent hard fault on ARM
     uint16_t value = pBuf[1];
     value <<= 8;
     value |= pBuf[0];
     if(pSubidx->Type == objPinNPN)
         value = ~value;
-
-    hal_pwm_write(pwm_port2cfg[pSubidx->Base], value);
+    
+    hal_pwm_write(pSubidx->Base, value);
 
     return MQTTSN_RET_ACCEPTED;
 }
 
-// Register PWM Object
 e_MQTTSN_RETURNS_t pwmRegisterOD(indextable_t *pIdx)
 {
     uint16_t base = pIdx->sidx.Base;
-    if(pwmCheckBase(base) != 0)
+    uint8_t dio = hal_pwm_base2dio(base);
+    
+    if(dioCheckBase(dio) != 0)
         return MQTTSN_RET_REJ_INV_ID;
-
-    // configure port and mark as busy
-    dioConfigure(pwm_port2dio[base], DIO_MODE_PWM);
-
-    uint8_t cfg = pwm_port2cfg[base];
-
-    if(pIdx->sidx.Type == objPinPNP)
-        hal_pwm_write(cfg, 0);
-    else
-        hal_pwm_write(cfg, 0xFFFF);
+    
+    dioTake(dio);
+    hal_pwm_configure(base);
+    
+    if(pIdx->sidx.Type == objPinNPN)
+        hal_pwm_write(base, 0xFFFF);
 
     pIdx->cbWrite = &pwmWriteOD;
-
+    
     return MQTTSN_RET_ACCEPTED;
 }
 
 void pwmDeleteOD(subidx_t * pSubidx)
 {
     uint16_t base = pSubidx->Base;
+    uint8_t dio = hal_pwm_base2dio(base);
     
-    if(pwmCheckBase(base) != 1)
-        return;
-    
-    hal_pwm_write(pwm_port2cfg[base], 0);
-    base = pwm_port2dio[base];
-
-    // Release PIN
-    dioConfigure(base, DIO_MODE_IN_FLOAT);
+    hal_pwm_delete(base);
+    dioRelease(hal_pwm_base2dio(base));
 }
-*/
-
 
 #endif  // ((defined EXTDIO_USED) && (defined EXTPWM_USED))
 
